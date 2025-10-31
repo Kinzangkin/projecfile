227fa110b428c27ce8b7d99610e1494ebecf7b88
Kinzangkin <franclinpondaag@gmail.com>
2025-11-01 07:19:19 +0800

    add a script for ads

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Main from "@/components/Main";
 import TIps from "@/components/TIps";
+import Script from "next/script";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <div>
         <Footer />
       </div>
+      <Script type='text/javascript' src='//pl27963750.effectivegatecpm.com/79/92/dd/7992dd6f82a2fcd3f7d7e5ccfb9d2eee.js'/>
     </div>
   );
 }
